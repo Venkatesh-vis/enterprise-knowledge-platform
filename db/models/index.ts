9e22bbec30d2dd593475a2803cdb8a766c4bb44c
Venkatesh Vishwanadula <103623631+Venkatesh-vis@users.noreply.github.com>
2026-09-22 15:53:10 +0530

    feat: register invitation model

--- db/models/index.ts
+++ db/models/index.ts
@@ -6,91 +6,30 @@ import Role from "./role";
 import Permission from "./permission";
 import RolePermission from "./role-permission";
 import AuditLog from "./audit-log";
+import Invitation from "./invitation";
 
-User.hasMany(OrganizationMembership, {
-  foreignKey: "userId",
-  as: "memberships",
-});
+User.hasMany(OrganizationMembership, { foreignKey: "userId", as: "memberships" });
+Organization.hasMany(OrganizationMembership, { foreignKey: "organizationId", as: "memberships" });
+OrganizationMembership.belongsTo(User, { foreignKey: "userId", as: "user" });
+OrganizationMembership.belongsTo(Organization, { foreignKey: "organizationId", as: "organization" });
+OrganizationMembership.belongsTo(Role, { foreignKey: "roleId", as: "role" });
+Role.hasMany(OrganizationMembership, { foreignKey: "roleId", as: "memberships" });
+Role.hasMany(RolePermission, { foreignKey: "roleId", as: "rolePermissions" });
+Permission.hasMany(RolePermission, { foreignKey: "permissionId", as: "rolePermissions" });
+RolePermission.belongsTo(Role, { foreignKey: "roleId", as: "role" });
+RolePermission.belongsTo(Permission, { foreignKey: "permissionId", as: "permission" });
+Organization.hasMany(Role, { foreignKey: "organizationId", as: "roles" });
+Role.belongsTo(Organization, { foreignKey: "organizationId", as: "organization" });
+AuditLog.belongsTo(User, { foreignKey: "actorUserId", as: "actor" });
+AuditLog.belongsTo(User, { foreignKey: "targetUserId", as: "targetUser" });
+AuditLog.belongsTo(Organization, { foreignKey: "organizationId", as: "organization" });
+User.hasMany(AuditLog, { foreignKey: "actorUserId", as: "auditLogs" });
+Organization.hasMany(AuditLog, { foreignKey: "organizationId", as: "auditLogs" });
 
-Organization.hasMany(OrganizationMembership, {
-  foreignKey: "organizationId",
-  as: "memberships",
-});
-
-OrganizationMembership.belongsTo(User, {
-  foreignKey: "userId",
-  as: "user",
-});
-
-OrganizationMembership.belongsTo(Organization, {
-  foreignKey: "organizationId",
-  as: "organization",
-});
-
-OrganizationMembership.belongsTo(Role, {
-  foreignKey: "roleId",
-  as: "role",
-});
-
-Role.hasMany(OrganizationMembership, {
-  foreignKey: "roleId",
-  as: "memberships",
-});
-
-Role.hasMany(RolePermission, {
-  foreignKey: "roleId",
-  as: "rolePermissions",
-});
-
-Permission.hasMany(RolePermission, {
-  foreignKey: "permissionId",
-  as: "rolePermissions",
-});
-
-RolePermission.belongsTo(Role, {
-  foreignKey: "roleId",
-  as: "role",
-});
-
-RolePermission.belongsTo(Permission, {
-  foreignKey: "permissionId",
-  as: "permission",
-});
-
-Organization.hasMany(Role, {
-  foreignKey: "organizationId",
-  as: "roles",
-});
-
-Role.belongsTo(Organization, {
-  foreignKey: "organizationId",
-  as: "organization",
-});
-
-AuditLog.belongsTo(User, {
-  foreignKey: "actorUserId",
-  as: "actor",
-});
-
-AuditLog.belongsTo(User, {
-  foreignKey: "targetUserId",
-  as: "targetUser",
-});
-
-AuditLog.belongsTo(Organization, {
-  foreignKey: "organizationId",
-  as: "organization",
-});
-
-User.hasMany(AuditLog, {
-  foreignKey: "actorUserId",
-  as: "auditLogs",
-});
-
-Organization.hasMany(AuditLog, {
-  foreignKey: "organizationId",
-  as: "auditLogs",
-});
+Invitation.belongsTo(Organization, { foreignKey: "organizationId", as: "organization" });
+Invitation.belongsTo(User, { foreignKey: "invitedByUserId", as: "invitedBy" });
+Invitation.belongsTo(Role, { foreignKey: "roleId", as: "role" });
+Organization.hasMany(Invitation, { foreignKey: "organizationId", as: "invitations" });
 
 export {
   User,
@@ -101,4 +40,5 @@ export {
   Permission,
   RolePermission,
   AuditLog,
+  Invitation,
 };
